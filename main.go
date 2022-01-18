1896aaef209dea989fcb706508ce03f157dcb7b9
redrover9 <grace.thompson97@gmail.com>
2022-01-18 15:38:38 -0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"net"
+	"time"
+)
+
+var (
+	device      string = "enp4s0"
+	snapshotLen int32  = 1024
+	promiscuous bool   = false
+	err         error
+	timeout     time.Duration = 30 * time.Second
+	handle      *pcap.Handle
+	buffer      gopacket.SerializeBuffer
+	options     gopacket.SerializeOptions
+)
+
+func main() {
+
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	rawBytes := []byte{10, 20, 30}
+
+	ipLayer := &layers.IPv4{
+		SrcIP: net.IP{127, 0, 0, 1},
+		DstIP: net.IP{8, 8, 8, 8},
+	}
+	ethernetLayer := &layers.Ethernet{
+		SrcMAC: net.HardwareAddr{0xFF, 0xAA, 0xFA, 0xAA, 0xFF, 0xAA},
+		DstMAC: net.HardwareAddr{0xBD, 0xBD, 0xBD, 0xBD, 0xBD, 0xBD},
+	}
+	tcpLayer := &layers.TCP{
+		SrcPort: layers.TCPPort(4321),
+		DstPort: layers.TCPPort(80),
+	}
+
+	buffer = gopacket.NewSerializeBuffer()
+	gopacket.SerializeLayers(buffer, options,
+		ethernetLayer,
+		ipLayer,
+		tcpLayer,
+		gopacket.Payload(rawBytes),
+	)
+	outgoingPacket := buffer.Bytes()
+	err = handle.WritePacketData(outgoingPacket)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
